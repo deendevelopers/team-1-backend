6f8738b22f872c0012d2f893a288f95d3182f874
Shahoor Malik <shahoormalik@gmail.com>
2019-06-22 16:08:59 +0100

    fr update

--- fr/models.py
+++ fr/models.py
@@ -8,5 +8,6 @@ class mosque_main(models.Model):
 	borough = models.CharField(max_length=1000)
 	area = models.CharField(max_length=1000)
 	name = models.CharField(max_length=1000)
+	address = models.CharField(max_length=1000)
 	postcode = models.CharField(max_length=1000)
 	website = models.CharField(max_length=1000)
